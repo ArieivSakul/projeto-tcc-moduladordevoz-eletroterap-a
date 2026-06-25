db6c461f06bdc7b977954da36b3d3608a8d257e7
Lucas Alves Vieira <83738829+ArieivSakul@users.noreply.github.com>
2026-06-25 11:16:28 -0300

    Delete script.sh

--- script.sh
+++
@@ -1,6 +0,0 @@
-git init
-git add .
-git commit -m "feat: adicionar estrutura inicial do TCC com codigo e monografia"
-git branch -M main
-git remote add origin https://github.com/SEU_USUARIO/NOME_DO_REPOSITORIO.git
-git push -u origin main
